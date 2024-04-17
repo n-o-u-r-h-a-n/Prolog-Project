b9cc4d9918c6e325aba11d4c04616702a9058b79
Nourhan Islam Shata <163640161+n-o-u-r-h-a-n@users.noreply.github.com>
2024-04-17 12:39:13 +0200

    Delete Team_X.pl

--- Project Knowledge base and template/Team_X.pl
+++
@@ -1,8 +0,0 @@
-% This line consults the knowledge bases from this file,
-% instead of needing to consult the files individually.
-% This line MUST be included in the final submission.
-:- ['transport_kb', 'slots_kb'].
-
-earliest_slot(Group, Week, Day, H):-
-    day_slots(Group, Week, Day , [H|T]).
-   
