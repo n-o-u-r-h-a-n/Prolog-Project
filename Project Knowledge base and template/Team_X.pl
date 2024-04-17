ded67510e3774bfe8cd005131f54954ed610a371
Ahmed Tamer <ahmedtamer930@gmail.com>
2024-04-17 11:57:24 +0200

    group_days method

--- Project Knowledge base and template/Team_X.pl
+++ Project Knowledge base and template/Team_X.pl
@@ -2,3 +2,9 @@
 % instead of needing to consult the files individually.
 % This line MUST be included in the final submission.
 :- ['transport_kb', 'slots_kb'].
+
+group_helper(Group,day_timing(Week, Day)):-
+    scheduled_slot(Week, Day, _, _, Group).
+
+group_days(Group, List):-
+    setof(day_timing(Week,Day), group_helper(Group,day_timing(Week, Day)), List).
