b348157216f79afd318fbbe776afaa0478902a25
Nourhan Islam Shata <163640161+n-o-u-r-h-a-n@users.noreply.github.com>
2024-04-17 12:19:24 +0200

    earliest slot predicate

--- Project Knowledge base and template/Team_X.pl
+++ Project Knowledge base and template/Team_X.pl
@@ -2,3 +2,7 @@
 % instead of needing to consult the files individually.
 % This line MUST be included in the final submission.
 :- ['transport_kb', 'slots_kb'].
+
+earliest_slot(Group, Week, Day, H):-
+    day_slots(Group, Week, Day , [H|T]).
+   
