4fe462326b92c4f7986759f9e5671f9892436604
HamadaTamer <141588693+HamadaTamer@users.noreply.github.com>
2024-04-17 11:38:07 +0200

    Starting files

---
+++ Project Knowledge base and template/Team_X.pl
@@ -0,0 +1,4 @@
+% This line consults the knowledge bases from this file,
+% instead of needing to consult the files individually.
+% This line MUST be included in the final submission.
+:- ['transport_kb', 'slots_kb'].
